70db6c410baf579115edbcb402dbf0d9b8baa38a
Kirill Ki <kirillkalapov@gmail.com>
2021-06-03 11:51:16 +0200

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by  Mr.Ki on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
